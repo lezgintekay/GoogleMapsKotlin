875ac288863f04af2c996ddbefb49ac26c62a0f7
Lezgin Tekay <tekay.lezgin@yandex.com>
2022-12-08 20:30:19 +0300

    Received last known location from user

--- app/src/main/java/com/lezgintekay/googlemapskotlin/MapsActivity.kt
+++ app/src/main/java/com/lezgintekay/googlemapskotlin/MapsActivity.kt
@@ -1,7 +1,16 @@
 package com.lezgintekay.googlemapskotlin
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -15,6 +24,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private lateinit var locationManager : LocationManager
+    private lateinit var locationListener : LocationListener
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,20 +40,64 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
+    @SuppressLint("MissingPermission")
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
+        /*
         val ankara = LatLng(39.920896,32.8523106)
         mMap.addMarker(MarkerOptions().position(ankara).title("Ankara Kızılay"))
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ankara, 15f))
+        */
+
+        // casting -> as
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        locationListener = object : LocationListener {
+            override fun onLocationChanged(p0: Location) {
+                mMap.clear()
+                val currentLocation = LatLng(p0.latitude,p0.longitude)
+                mMap.addMarker(MarkerOptions().position(currentLocation).title("You are here now"))
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation,15f))
+            }
+
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),1)
+
+        }else {
+
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1, 1f, locationListener)
+            val lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            if (lastKnownLocation != null) {
+                val lastKnownLatLng = LatLng(lastKnownLocation.latitude, lastKnownLocation.longitude)
+                mMap.addMarker(MarkerOptions().position(lastKnownLatLng).title("Last Known Location"))
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastKnownLatLng,15f))
+            }
+        }
+
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode ==1) {
+
+            if (grantResults.size > 0) {
+
+                if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED) {
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1, 1f, locationListener)
+                }
+
+            }
+
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
